d8511a17287cf5eb9d6838d9463a8a4268c9cde6
DeveloperBurak <burakarasu350@gmail.com>
2020-01-04 13:36:16 +0300

    code formats

--- src/libs/mslinks/ShellLinkException.java
+++ src/libs/mslinks/ShellLinkException.java
@@ -15,11 +15,11 @@
 package mslinks;
 
 public class ShellLinkException extends Exception {
-	public ShellLinkException() {
-		super();
-	}
-	
-	public ShellLinkException(String msg) {
-		super(msg);
-	}
+    public ShellLinkException() {
+        super();
+    }
+
+    public ShellLinkException(String msg) {
+        super(msg);
+    }
 }
